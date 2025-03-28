d8d08cbd7898be588038c62ef1c75d920fda0c03
88GOrszak <gabriel.orszak@axisgroup.eu>
2025-03-28 16:01:19 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,32 @@
 # Import python packages
 import streamlit as st
 import requests
-import pandas as pd_df
+import pandas as pd
 from snowflake.snowpark.functions import col
+ 
 
 # Write directly to the app
-st.title(":tropical_drink: Customize Your Smoothie! :tropical_drink:")
+st.title(" :tropical_drink: Customize Your Smoothie! :tropical_drink:")
 st.write("Choose the fruits you want in your custom Smoothie!:")
-
-
+ 
 name_on_order = st.text_input('Name on Smoothie')
 st.write('The name on your Smoothie will be:', name_on_order)
-
+ 
 cnx = st.connection("snowflake")
 session = cnx.session()
+ 
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
+ 
+pd_df=my_dataframe.to_pandas()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+# st.dataframe(pd_df)
+# st.stop()
 
-ingredients_list = st.multiselect('Choose up to 5 ingredients',my_dataframe, max_selections = 6)
+
+ingredients_list = st.multiselect('Choose up to 5 ingredients',my_dataframe, max_selections = 5)
+ 
 
 if ingredients_list:
  
@@ -48,6 +56,4 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
 
         st.success('Your Smoothie is ordered', icon="✅")
-
-
 
